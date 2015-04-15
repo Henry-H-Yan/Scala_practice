da4b30a574a9e4a263fd3d387b3f9781ef4bc280
Henry-H-Yan <henryyan829@gmail.com>
2015-04-15 00:15:15 -0400

    new scala practice

--- .worksheet/src/greeter.sheet.scala
+++ .worksheet/src/greeter.sheet.scala
@@ -5,5 +5,48 @@ object sheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._;
   
     val x = 5;System.out.println("""x  : Int = """ + $show(x ));$skip(31); 
   def increase(i: Int) = i + 1;System.out.println("""increase: (i: Int)Int""");$skip(14); val res$0 = 
-  increase(x);System.out.println("""res0: Int = """ + $show(res$0))}
+  increase(x);System.out.println("""res0: Int = """ + $show(res$0));$skip(37); 
+  
+  def multiple(x:Int)(y:Int) =x*y;System.out.println("""multiple: (x: Int)(y: Int)Int""");$skip(18); val res$1 = 
+  multiple(3)(34);System.out.println("""res1: Int = """ + $show(res$1));$skip(90); 
+  
+  
+  
+  lazy val fs: Stream[Int] =
+  0 #:: 1 #:: fs.zip(fs.tail).map(p => p._1 + p._2);System.out.println("""fs: => Stream[Int]""");$skip(264); 
+
+
+/**
+
+ the prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+def factors(n: Long): List[Long] =
+  (2 to math.sqrt(n).toInt).find(n % _ == 0)
+    .map(i => i.toLong :: factors(n / i)).getOrElse(List(n));System.out.println("""factors: (n: Long)List[Long]""");$skip(58); 
+
+val r = factors(600851475143L).last;System.out.println("""r  : Long = """ + $show(r ));$skip(295);  // last of the sieve
+
+
+
+     def primesUnder(n: Int): List[Int] = {
+        require(n >= 2)
+
+      def rec(i: Int, primes: List[Int]): List[Int] = {
+          if (i >= n) primes
+          else if (prime(i, primes)) rec(i + 1, i :: primes)
+             else rec(i + 1, primes)
+          }
+
+  rec(2, List()).reverse
+};System.out.println("""primesUnder: (n: Int)List[Int]""");$skip(81); 
+
+def prime(num: Int, factors: List[Int]): Boolean = factors.forall(num % _ != 0);System.out.println("""prime: (num: Int, factors: List[Int])Boolean""");$skip(68); 
+
+
+
+val res = fs.view.takeWhile(_ <= 4000000).filter(_ % 2 == 0).sum;System.out.println("""res  : Int = """ + $show(res ))}
+
 }
--- src/greeter/sheet.sc
+++ src/greeter/sheet.sc
@@ -6,4 +6,52 @@ object sheet {
     val x = 5                                     //> x  : Int = 5
   def increase(i: Int) = i + 1                    //> increase: (i: Int)Int
   increase(x)                                     //> res0: Int = 6
+  
+  def multiple(x:Int)(y:Int) =x*y                 //> multiple: (x: Int)(y: Int)Int
+  multiple(3)(34)                                 //> res1: Int = 102
+  
+  
+  
+  lazy val fs: Stream[Int] =
+  0 #:: 1 #:: fs.zip(fs.tail).map(p => p._1 + p._2)
+                                                  //> fs: => Stream[Int]
+
+
+/**
+
+ the prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+def factors(n: Long): List[Long] =
+  (2 to math.sqrt(n).toInt).find(n % _ == 0)
+    .map(i => i.toLong :: factors(n / i)).getOrElse(List(n))
+                                                  //> factors: (n: Long)List[Long]
+
+val r = factors(600851475143L).last // last of the sieve
+                                                  //> r  : Long = 6857
+
+
+
+     def primesUnder(n: Int): List[Int] = {
+        require(n >= 2)
+
+      def rec(i: Int, primes: List[Int]): List[Int] = {
+          if (i >= n) primes
+          else if (prime(i, primes)) rec(i + 1, i :: primes)
+             else rec(i + 1, primes)
+          }
+
+  rec(2, List()).reverse
+}                                                 //> primesUnder: (n: Int)List[Int]
+
+def prime(num: Int, factors: List[Int]): Boolean = factors.forall(num % _ != 0)
+                                                  //> prime: (num: Int, factors: List[Int])Boolean
+
+
+
+val res = fs.view.takeWhile(_ <= 4000000).filter(_ % 2 == 0).sum
+                                                  //> res  : Int = 4613732
+
 }
